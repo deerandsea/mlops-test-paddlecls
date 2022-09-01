16f2895930022b88d729e6c8257c240c8f281397
deerandsea <mr.rikudu@gmail.com>
2022-09-01 09:23:51 +0800

    .

--- mlflow-train.py
+++ mlflow-train.py
@@ -71,6 +71,8 @@ if __name__ == "__main__":
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_param("deer1", 111)
+        mlflow.log_param("deer2", 111)
+        mlflow.log_param("deer3", 111)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
